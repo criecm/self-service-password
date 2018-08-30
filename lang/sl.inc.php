b6ee9cdc92bfc9998fa774c328e6117b63cd34ee
Geoffroy Desvernay <dgeo@centrale-marseille.fr>
2018-08-30 15:49:58 +0200

    duplicates

--- lang/sl.inc.php
+++ lang/sl.inc.php
@@ -132,4 +132,3 @@ $messages['sshkeyerror'] = "SSH Ključna je bila zavrnjena z imeniku LDAP";
 $messages['pwned'] = "Your new password has already been published on leaks, you should consider changing it on any other service that it is in use";
 $messages['policypwned'] = "Your new password may not be published on any previous public password leak from any site";
 $messages['throttle'] = "Too fast !!! Please try again later (if ever you are human)";
-$messages['throttle'] = "Too fast !!! Please try again later (if ever you are human)";
